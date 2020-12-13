6633241cb7813d08ee559c474f5b0cce6bb89118
SvetkaSokolova <ssokolova90@inbox.ru>
2020-12-13 21:07:27 +0300

    logo

--- .vuepress/theme/components/MainPicture/MainPicture.vue
+++ .vuepress/theme/components/MainPicture/MainPicture.vue
@@ -1,13 +1,18 @@
 <template>
 
-  <div :class="$blockClassName">
+  <div
+    :class="$blockClassName"
+    @click="nextLogo()"
+    :style="{backgroundImage: 'url(' + logo.picture + ')' }">
 
-    <img src="/images/assets/main.jpg">
 
     <div :class="$e('title')">
-      Международный патриотический проект «Парад Памяти»
+      {{ logo.title }}
     </div>
 
+<!--    <div v-for="logo in logoList">-->
+<!--      {{ JSON.stringify(logo) }}-->
+<!--    </div>-->
 
   </div>
 
@@ -16,9 +21,54 @@
 <script>
   export default {
     name: 'MainPicture',
+    data() {
+      return {
+        selectedLogo: 0
+      }
+    },
     computed: {
 
-    }
+      logo() {
+
+        if (this.selectedLogo  < this.logoList.length) {
+          return this.logoList[+this.selectedLogo];
+        }
+        return {
+          title: 'Международный патриотический проект «Парад Памяти»',
+          picture: '/images/assets/main.jpg'
+        }
+      },
+
+      logoList() {
+
+        let logoList = this.$pages('MainLogo').map(logo => {
+
+          if (!logo || !logo.frontmatter) {
+            return {}
+          }
+
+          return {
+            title: logo.title,
+            picture: logo.frontmatter.picture
+          };
+        });
+
+        console.log('LogoList', logoList);
+
+        return logoList;
+      }
+
+    },
+    methods: {
+      nextLogo() {
+
+        let nextSelected = this.selectedLogo + 1;
+        if (nextSelected >= this.logoList.length) {
+          nextSelected = 0;
+        }
+        this.selectedLogo = nextSelected;
+      }
+    },
   }
 </script>
 
